42e715aeb83f0e2d9132763bbd8dcf5b6f0ab9c9
DanielH4 <danne.halvarsson@gmail.com>
2022-08-23 15:51:29 +0200

    add model for chess games

--- apps/chessgames/models.py
+++ apps/chessgames/models.py
@@ -1,3 +1,24 @@
+import json
+
 from django.db import models
 
-# Create your models here.
+from chesslib.api import Chess
+
+
+class ChessGame(models.Model):
+    Color = models.TextChoices('Color', 'white black')
+    turn = models.CharField(choices=Color.choices, max_length=5)
+    in_check = models.BooleanField()
+    checkmate = models.BooleanField()
+    board = models.JSONField()
+
+    @classmethod
+    def create_default_game(cls):
+        game = Chess()
+
+        return cls(
+            turn=game.turn,
+            in_check=game.in_check,
+            checkmate=game.checkmate,
+            board = json.loads(game.toJSON()).get('board').get('board')
+        )
