1f7d6d1af6518fb23f591309ced73f9c11416822
DanielH4 <danne.halvarsson@gmail.com>
2022-08-25 01:05:37 +0200

    add initial views for ChessGame models

--- apps/chessgames/views.py
+++ apps/chessgames/views.py
@@ -1,3 +1,32 @@
-from django.shortcuts import render
+from rest_framework import viewsets, status
+from rest_framework.decorators import action
+from rest_framework.response import Response
 
-# Create your views here.
+from apps.chessgames.models import ChessGame
+from apps.chessgames.serializers import ChessGameSerializer
+
+
+class ChessGameViewSet(viewsets.ModelViewSet):
+    queryset = ChessGame.objects.all()
+    serializer_class = ChessGameSerializer
+
+    @action(methods=['post'],
+            detail=True)
+    def move(self, request, from_square, to_square, *args, **kwargs):
+        """
+        Performs a legal move and updates instance.
+        """
+        instance = self.get_object()
+        chess = instance.to_chess()
+
+        # handle invalid move
+        if chess.move(from_square, to_square) is None:
+            return Response(data={'message': 'Invalid move.'}, status=status.HTTP_400_BAD_REQUEST)
+
+        updated_instance = ChessGame.from_chess(chess)
+        updated_instance_serializer = ChessGameSerializer(updated_instance)
+        serializer = ChessGameSerializer(instance, data=updated_instance_serializer.data)
+        serializer.is_valid()
+        serializer.save()
+
+        return Response(serializer.data)
