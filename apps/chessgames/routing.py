6b5dbf1ce15a2917f85f68ca698973714ee2cef8
DanielH4 <danne.halvarsson@gmail.com>
2022-09-09 17:55:57 +0200

    update websocket urlpatterns for chess games

--- apps/chessgames/routing.py
+++ apps/chessgames/routing.py
@@ -2,9 +2,9 @@ from django.urls import path
 
 from rest_framework.urlpatterns import format_suffix_patterns
 
-from apps.chessgames.consumers import ChessGameConsumer
+from apps.chessgames.consumers import AnonymousChessGameConsumer
 
 
 websocket_urlpatterns = format_suffix_patterns([
-    path('ws/chessgames/<int:game_id>/', ChessGameConsumer.as_asgi()),
+    path('ws/chessgames/anonymous/<int:game_id>/', AnonymousChessGameConsumer.as_asgi()),
 ])
