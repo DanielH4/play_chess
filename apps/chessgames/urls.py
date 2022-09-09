d7ab67e6c09148607e506b8fcd9757bfdd93ac22
DanielH4 <danne.halvarsson@gmail.com>
2022-09-09 17:49:14 +0200

    update chessgames urlfconf with anonymous games

--- apps/chessgames/urls.py
+++ apps/chessgames/urls.py
@@ -2,7 +2,10 @@ from django.urls import path
 
 from rest_framework.urlpatterns import format_suffix_patterns
 
-from apps.chessgames.views import ChessGameViewSet
+from apps.chessgames.views import (
+    AnonymousChessGameViewSet,
+    ChessGameViewSet,
+)
 
 
 chessgame_list = ChessGameViewSet.as_view({
@@ -19,6 +22,14 @@ chessgame_move = ChessGameViewSet.as_view({
 chessgame_legal_moves = ChessGameViewSet.as_view({
     'get': 'legal_moves'
 })
+anon_chessgame_list = AnonymousChessGameViewSet.as_view({
+    'get': 'list',
+    'post': 'create',
+})
+anon_chessgame_detail = AnonymousChessGameViewSet.as_view({
+    'get': 'retrieve',
+    'delete': 'destroy',
+})
 
 urlpatterns = format_suffix_patterns([
     path('chessgames/', chessgame_list),
@@ -26,4 +37,6 @@ urlpatterns = format_suffix_patterns([
     path('chessgames/<int:pk>/legal-moves/', chessgame_legal_moves),
     path('chessgames/<int:pk>/legal-moves/<str:square>/', chessgame_legal_moves),
     path('chessgames/<int:pk>/move/<str:from_square>/<str:to_square>/', chessgame_move),
+    path('chessgames/anonymous/', anon_chessgame_list),
+    path('chessgames/anonymous/<int:pk>/', anon_chessgame_detail),
 ])
